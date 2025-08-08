672498200245e80df31d3b5ed788edaed011fdd9
brian <dpi.studio@gmail.com>
2025-08-08 22:01:20 +0800

    feat: 更新 ReviewView 組件，新增輸入功能以添加項目並顯示項目列表

--- src/views/ReviewView.vue
+++ src/views/ReviewView.vue
@@ -1,5 +1,34 @@
 <template>
-  <div class="review">
-    <h1>This is a review page</h1>
+  <div>
+    <input type="text" v-model="newName" />
+    {{ newName }}
+    <input type="number" v-model.number="newNumber" />
+    {{ newNumber }}
+    <button type="button" @click="addItem">Add</button>
   </div>
+  <ul>
+    <li v-for="(item, key) in items" :key="item.name">
+      {{ key + 1 }}. {{ item.name }} ({{ item.number }})
+    </li>
+  </ul>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+const newName = ref('');
+const newNumber = ref(0);
+
+const addItem = () => {
+  items.value.push({
+    name: newName.value,
+    number: newNumber.value
+  });
+}
+
+const items = ref([
+  { name: 'Item 1', number: 1 },
+  { name: 'Item 2', number: 2 },
+  { name: 'Item 3', number: 3 }
+]);
+</script>
