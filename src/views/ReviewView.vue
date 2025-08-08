7489d81ff74053cd16b478edc11e879492b608e6
brian <dpi.studio@gmail.com>
2025-08-08 21:11:37 +0800

    feat: 新增 ReviewView 組件以顯示評價頁面

---
+++ src/views/ReviewView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="review">
+    <h1>This is a review page</h1>
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .review {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
