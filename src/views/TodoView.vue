6614399c56d56fff9b76408ec3c548e82cf69682
brian <dpi.studio@gmail.com>
2025-08-09 15:40:02 +0800

    feat: 新增註冊表單及相關數據綁定

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,9 +1,22 @@
 <template>
   <h1>{{ pageTitle }}</h1>
+  <h2>Sign Up</h2>
+  <input type="text" v-model="signUpModel.email">
+  <input type="text" v-model="signUpModel.password">
+  <input type="text" v-model="signUpModel.nickname">
+  <button type="button">Sign Up</button>
+  <br />
+  {{ signUpModel }}
 </template>
 
 <script setup>
 import { ref } from 'vue';
 
 const pageTitle = ref('Todo');
+
+const signUpModel = ref({
+  email: 'Brian',
+  password: 'P@ssw0rd',
+  nickname: 'dpi'
+});
 </script>
