db4116fc2255a5b9867a2420954cbda4db880ae4
brian <dpi.studio@gmail.com>
2025-08-09 17:05:06 +0800

    feat: 在 TodoView 組件中新增登入功能及相關數據綁定

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -1,15 +1,25 @@
 <template>
   <h1>{{ pageTitle }}</h1>
+
   <h2>Sign Up</h2>
   <input type="text" v-model="signUpModel.email">
   <input type="password" v-model="signUpModel.password">
   <input type="text" v-model="signUpModel.nickname">
   <button type="button" @click="signUp">Sign Up</button>
   <button type="button" @click="setRandomEmail">Random</button>
-  <br />
+  <hr />
   {{ signUpModel }}
-  <br />
+  <hr />
   {{ signUpRes }}
+
+  <h2>Sign In</h2>
+  <input type="text" v-model="signInModel.email">
+  <input type="password" v-model="signInModel.password">
+  <button type="button" @click="signIn">Sign In</button>
+  <hr />
+  {{ signInModel }}
+  <hr />
+  {{ signInRes }}
 </template>
 
 <script setup>
@@ -18,23 +28,14 @@ import axios from 'axios';
 
 const pageTitle = ref('Todo');
 const baseURL = 'https://todolist-api.hexschool.io/';
+
 const signUpRes = ref('');
+const signInRes = ref('');
 
 const signUpModel = ref({
   email: '',
   password: '',
   nickname: ''
-});
-
-const setRandomEmail = () => {
-  let rnd = Math.random().toString(36);
-  signUpModel.value.email = `${rnd.substring(2, 8)}@gmail.com`;
-  signUpModel.value.password = rnd.substring(2, 8);
-  signUpModel.value.nickname = rnd.substring(2, 4);
-}
-
-onMounted(() => {
-  setRandomEmail();
 });
 
 const signUp = async () => {
@@ -48,4 +49,35 @@ const signUp = async () => {
     console.error(error);
   }
 }
+
+const signInModel = ref({
+  email: '',
+  password: ''
+});
+
+const signIn = async () => {
+  try {
+    let api = `${baseURL}users/sign_in`;
+    const res = await axios.post(api, signInModel.value);
+    console.log(res);
+    signInRes.value = res.data;
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+const setRandomEmail = () => {
+  let rnd = Math.random().toString(36);
+  signUpModel.value.email = `${rnd.substring(2, 8)}@gmail.com`;
+  signUpModel.value.password = rnd.substring(2, 8);
+  signUpModel.value.nickname = rnd.substring(2, 4);
+
+  signInModel.value.email = signUpModel.value.email;
+  signInModel.value.password = signUpModel.value.password;
+}
+
+onMounted(() => {
+  setRandomEmail();
+});
 </script>
