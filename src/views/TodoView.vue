1fa66e62b8580712f2aad476c5cfa2ec8f2b291a
brian <dpi.studio@gmail.com>
2025-08-09 15:57:34 +0800

    feat: 在 TodoView 組件中新增註冊功能的按鈕事件處理

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -4,7 +4,7 @@
   <input type="text" v-model="signUpModel.email">
   <input type="text" v-model="signUpModel.password">
   <input type="text" v-model="signUpModel.nickname">
-  <button type="button">Sign Up</button>
+  <button type="button" @click="signUp">Sign Up</button>
   <br />
   {{ signUpModel }}
 </template>
@@ -13,10 +13,15 @@
 import { ref } from 'vue';
 
 const pageTitle = ref('Todo');
+const baseURL = 'https://todolist-api.hexschool.io/';
 
 const signUpModel = ref({
   email: 'Brian',
   password: 'P@ssw0rd',
   nickname: 'dpi'
 });
+
+const signUp = async () => {
+  console.log(`${baseURL}users/sign_up`);
+}
 </script>
