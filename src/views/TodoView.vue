c198c0096c00d1ff6b4996d321949bc851a4464c
brian <dpi.studio@gmail.com>
2025-08-09 16:36:11 +0800

    feat: 修改註冊表單，將密碼輸入框類型更改為密碼，並清除預設值

--- src/views/TodoView.vue
+++ src/views/TodoView.vue
@@ -2,26 +2,42 @@
   <h1>{{ pageTitle }}</h1>
   <h2>Sign Up</h2>
   <input type="text" v-model="signUpModel.email">
-  <input type="text" v-model="signUpModel.password">
+  <input type="password" v-model="signUpModel.password">
   <input type="text" v-model="signUpModel.nickname">
   <button type="button" @click="signUp">Sign Up</button>
+  <button type="button" @click="setRandomEmail">Random</button>
   <br />
   {{ signUpModel }}
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
+import axios from 'axios';
 
 const pageTitle = ref('Todo');
 const baseURL = 'https://todolist-api.hexschool.io/';
 
 const signUpModel = ref({
-  email: 'Brian',
-  password: 'P@ssw0rd',
-  nickname: 'dpi'
+  email: '',
+  password: '',
+  nickname: ''
+});
+
+const setRandomEmail = () => {
+  let rnd = Math.random().toString(36);
+  signUpModel.value.email = `${rnd.substring(2, 8)}@gmail.com`;
+  signUpModel.value.password = rnd.substring(2, 8);
+  signUpModel.value.nickname = rnd.substring(2, 4);
+}
+
+onMounted(() => {
+  setRandomEmail();
 });
 
 const signUp = async () => {
-  console.log(`${baseURL}users/sign_up`);
+  let api = `${baseURL}users/sign_up`;
+  console.log(api);
+  const res = await axios.post(api, signUpModel.value);
+  console.log(res);
 }
 </script>
